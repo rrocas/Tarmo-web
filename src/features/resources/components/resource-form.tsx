8b5bbce40f45ee558aa4bf69b52993690df97a95
rocas <rocasdev@proton.me>
2026-09-25 19:35:13 +0300

    fix: prevent resource form reset

--- src/features/resources/components/resource-form.tsx
+++ src/features/resources/components/resource-form.tsx
@@ -5,19 +5,32 @@ import { Input } from "@/components/ui/input"
 import { addResourceAction } from "../api/resource-actions"
 import { useRef } from "react"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { toast } from "sonner"
 
 export function ResourceForm() {
     const formRef = useRef<HTMLFormElement>(null)
 
-    const clientAction = async (formData: FormData) => {
-        await addResourceAction(formData)
-        formRef.current?.reset()
-    }
-
     return (
         <form
             ref={formRef}
-            action={clientAction}
+            onSubmit={async (event) => {
+                event.preventDefault()
+
+                const formData = new FormData(event.currentTarget)
+
+                try {
+                    await addResourceAction(formData)
+
+                    formRef.current?.reset()
+                    toast.success("Resource added to the list.")
+                } catch (error) {
+                    if (error instanceof Error) {
+                        toast.error(error.message)
+                    } else {
+                        toast.error("Something went wrong.")
+                    }
+                }
+            }}
             className="w-full pl-2 pr-3 pb-3 sticky top-16 z-30 flex space-x-2 bg-background"
         >
             <Input name="name" placeholder="Name *" required className="flex-1" />
